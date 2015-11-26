6c058071d698237b68534f58a53e4f051a5cac99
Waydrow <Waydrow@163.com>
2015-11-26 13:21:30 +0800

    完善js代码

--- js/index.js
+++ js/index.js
@@ -6,21 +6,21 @@
 var $docWidth = $(document).width();
 var isReady = false; //旋转地球动画是否加载完成
 $(document).ready(function(){
-    /*左侧中间文字滚动*/
+   /* /!*左侧中间文字滚动*!/
     $(".left-mid").myScroll({
         speed:40,
         rowHeight:$(".left-mid li").height()
     });
-    /*左侧下方文字滚动*/
+    /!*左侧下方文字滚动*!/
     $(".left-bottom-scroll").myScroll({
         speed:40,
         rowHeight:$(".left-bottom-scroll li").height()
     });
-    /*中间部分文字滚动*/
+    /!*中间部分文字滚动*!/
     $(".car-bottom-text").myScroll({
        speed: 40,
         rowHeight: $(".car-bottom-text li").height()
-    });
+    });*/
 
     /*中间车辆右侧停车场列表*/
     var $carList = $(".car-list li");
@@ -91,63 +91,3 @@ $(document).ready(function(){
     }
 
 })(jQuery);
-
-
-/*平行车辆页面*/
-$(document).ready(function () {
-    /*右上角*/
-    $(".right-top-park").hover(function(){
-        $(this).children('ul').stop(true,false).fadeIn();
-    },function(){
-        $(this).children('ul').stop(true,false).delay(300).fadeOut();
-    });
-
-    /*右下角*/
-    $(".right-bottom-park").hover(function(){
-        $(this).children('ul').delay(200).stop(true,false).delay(300).slideDown();
-    },function(){
-        $(this).children('ul').stop(true,false).delay(300).slideUp();
-    });
-
-    /*左下角*/
-    $(".progress-btn").on("click",function(){
-        $(this).toggleClass("progress-btn-on");
-        if($(this).hasClass("progress-btn-on")){
-            $(this).children('span').html("隐藏图例");
-            $(this).children('i').removeClass("icon-plus").addClass("icon-minus");
-        } else {
-            $(this).children('span').html("显示图例");
-            $(this).children('i').removeClass("icon-minus").addClass("icon-plus");
-        }
-        $(this).next("ul").stop(true,false).slideToggle();
-    })
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
