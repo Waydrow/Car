1c1a1cf09a7faf36bbc064dc63e2e67211c8f142
Waydrow <Waydrow@163.com>
2015-11-27 20:38:17 +0800

    模拟建设进度条js

--- js/park.js
+++ js/park.js
@@ -36,7 +36,8 @@ $(document).ready(function () {
             $('.main-list-park+p').html(rightTopMainContent[index]);
             /*左下方不显示，右下方显示图标按钮*/
             clearRightBottom();     /*右下方先隐藏全部按钮*/
-            clearTimeLineAndState();        /*隐藏右下角时间线*/
+            clearTimeLineAndState(); /*隐藏右下角时间线*/
+            clearRecommanAndBuild(); /*隐藏模拟建设中的弹出窗*/
             $(rightBottomBtn[index]).removeClass('hide');
             if(index==0||index==2)
                 $(rightBottomBtn).toggleClass('right-bottom-time-control');
@@ -177,7 +178,8 @@ $(document).ready(function () {
             else $('.parking-lot-state').removeClass('hide');
         })
     });
-    /*推荐方案*/
+
+    /*推荐方案与模拟建设*/
     /*创建滑动条*/
     var recommandWay = $(".recommand-way");
     var recommandWayContent = $(".recommand-way ul div");
@@ -189,41 +191,72 @@ $(document).ready(function () {
         });
     }
     var rightBottomSetting = $(".right-bottom-set li");
+    /*隐藏推荐方案与模拟建设弹出窗*/
+    function clearRecommanAndBuild(){
+        $(".park-pin").addClass('hide');    //隐藏大头针 P
+        $(".build-window").addClass('hide');    //隐藏建设窗口
+        $('.build-window input').addClass('hide'); //隐藏建设按钮
+        $(".recommand-way").addClass('hide');   //隐藏推荐弹出窗口
+        $(".build-window-confirm").addClass('hide'); //隐藏点击建设后的确认窗
+    }
     $(rightBottomSetting).each(function (index) {
        $(this).on('click',function(){
            $(this).parent().next().html($(this).html());
-           if(index==1)$(recommandWay).toggleClass('hide');
+           if(index==1){
+               clearRecommanAndBuild();
+               $(recommandWay).removeClass('hide');
+           }
+           else {
+               clearRecommanAndBuild();
+               $('.park-pin').removeClass('hide');
+               $('.build-window').removeClass('hide');
+           }
        });
     });
+
+
     /*推荐方案点击确定后*/
     $(".submit-recommand").on('click', function () {
-        var temp = 0;
+        var temp = 0,
+            dataValue = [], //红色条中的数字
+            redLine = $(".recommand-ranking ul .progress-bar"), //红色条
+            greenLine = $(".recommand-ranking ul .progress-bar+.progress-bar"); //绿色条
         for(var i=0; i< $(this).prev('ul').find('div.decrease-num').length; i++)
             temp+=$(this).prev('ul').find('div.decrease-num')[i].value;
         temp=temp*0.5;
-        $(".recommand-ranking ul .progress-bar+.progress-bar").eq(0).animate({
-            width: temp+"%"
-        });
-        $(".recommand-ranking ul .progress-bar").eq(0).animate({
-            width: ($(this).width()-temp)+'%'
-        });
-        var timerRanking = setInterval(function(){
-            $('.recommand-ranking ul .progress-num').eq(0)
-                .html($(".recommand-ranking ul .progress-bar").eq(0).width());
-        },200);
+        for(var i=0; i<redLine.length; i++)
+            dataValue[i] = $(redLine[i]).data('value');
+        /*绿色条增加*/
+        $(greenLine).each(function(index){
+            $(this).animate({
+                width: temp+'%'
+            },function(){
+                /*回调函数 条中的数字变化*/
+                $(this).children('.progress-num').html($(this).width());
+            });
 
+        });
+        /*红色条减小*/
+        $(redLine).each(function(index){
+            $(this).animate({
+                width: dataValue[index]-temp+'%'
+            },function(){
+                $(this).children('.progress-num').html($(this).width());
+            });
+        });
     });
 
-    /*左下角列表*/
+    /*左下角列表赋值*/
     !function () {
         var tempArray = [],
             temp = $('.progress-num');
-        for(var i=0; i<temp.length; i++){
-            tempArray[i] = ($(temp[i]).parent().width()/100)
-                *$(window).width()*($(temp[i]).parents('div.left-bottom-park').width()/100);
+        for(var i=temp.length-1; i>=0; i--){
+            tempArray[i] = $(temp[i]).parent().width();
+            tempArray[i]=$(window).width()*($($('.left-bottom-park')[1]).width()/100)
+                *(tempArray[i]/100);
         }
-        for( i=0; i<temp.length; i++){
-            $(temp[i]).html(Math.floor(tempArray[i]));
+        for(var j=temp.length-1; j>=0; j--) {
+            $(temp[j]).html(Math.floor(tempArray[j]));
         }
     }();
 
@@ -240,6 +273,16 @@ $(document).ready(function () {
         $(this).next("ul").stop(true,false).slideToggle();
     });
 
-
-
+    /*模拟建设相关*/
+    /*可编辑状态切换*/
+    $(".icon-pencil").on('click',function(){
+        $(this).siblings('input').toggleClass('hide');
+    });
+    /*点击建设后*/
+    $(".build-confirm-btn").on('click',function(){
+        $('.build-window-confirm').removeClass('hide');
+    });
+    $('.build-window-confirm input').on('click', function () {
+        $(this).parent().addClass('hide');
+    });
 });
